8b4db2301db99b8b2cf5f1457b91227695f880e1
saurabhshirgaokar <80784811+saurabhshirgaokar@users.noreply.github.com>
2026-04-24 13:12:40 -0500

    Update analysis.sql

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -1,3 +1,9 @@
-data/README.md
-notebooks/README.md
-visuals/README.md
+SELECT stay,
+       COUNT(*) AS count_int,
+       ROUND(AVG(todep),2) AS average_phq,
+       ROUND(AVG(tosc),2) AS average_scs,
+       ROUND(AVG(toas),2) AS average_as
+FROM students
+WHERE inter_dom = 'Inter'
+GROUP BY stay
+ORDER BY stay DESC;
